6d0907fdc090e5dc310a96868c7f926d5e1cd7a3
doctea <doctea@gmail.com>
2021-02-26 22:31:33 +0000

    shadow trigger+envelope gates on channel 11 notes 0-6 for benefit of bitbox

--- MidiOutput.hpp
+++ MidiOutput.hpp
@@ -1,57 +1,63 @@
 #ifndef MIDIOUTPUT_INCLUDED
 #define MIDIOUTPUT_INCLUDED
 
+#define MIDI_CHANNEL_BITBOX_OUT 11
+
 #include "MidiSetup.hpp"
 #include "Envelopes.h"    // for access to envelope info
 #include "Bass.hpp"       // for access to the bass channel info
 
 // functions for sending MIDI out
 
-void fire_trigger(byte t, byte v, bool internal = false) { 
-    //Serial.printf("firing trigger pitch=%i, v=%i\r\n", p, v);
-    // t = trigger number, p = keyboard note
-    byte p = MUSO_NOTE_MINIMUM + t;
-    if (
-      p>=MUSO_NOTE_MINIMUM && 
-      p<MUSO_NOTE_MAXIMUM) {
-        trigger_status[p - MUSO_NOTE_MINIMUM] = v>0; // TRIGGER_IS_ON;
-        MIDIOUT.sendNoteOn(p, v, MUSO_GATE_CHANNEL); //CHANNEL_DRUMS);  // output channel that the midimuso expects its triggers on
-    } else if (
-      //Serial.printf("is an envelope trigger!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-      p>=MUSO_NOTE_MAXIMUM && 
-      p<MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
-        update_envelope (p - (MUSO_NOTE_MAXIMUM), v, true);
-    } else if (p == MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
-      //Serial.printf(">> got BASS trigger!\r\n");
-      //if (bass.is_note_held()) // todo: make this so that can still play bass when no DAW present...
-        bass_note_on_and_next();
-      //else 
-      //  Serial.println("No note held? is_note_held is false");
-    } else {
-      Serial.printf("WARNING: fire_trigger not doing anything with pitch %i\r\n", p);
-    }
-    if (midiecho_enabled)
-      if (internal) echo_fire_trigger(p-MUSO_NOTE_MINIMUM, v);
+void fire_trigger(byte t, byte v, bool internal = false) {
+  //Serial.printf("firing trigger pitch=%i, v=%i\r\n", p, v);
+  // t = trigger number, p = keyboard note
+  byte p = MUSO_NOTE_MINIMUM + t;
+  if (
+    p >= MUSO_NOTE_MINIMUM &&
+    p < MUSO_NOTE_MAXIMUM) {
+    trigger_status[p - MUSO_NOTE_MINIMUM] = v > 0; // TRIGGER_IS_ON;
+    MIDIOUT.sendNoteOn(p, v, MUSO_GATE_CHANNEL);
+    MIDIOUT.sendNoteOn(t, v, MIDI_CHANNEL_BITBOX_OUT);
+  } else if (
+    //Serial.printf("is an envelope trigger!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    p >= MUSO_NOTE_MAXIMUM &&
+    p < MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
+    update_envelope (p - (MUSO_NOTE_MAXIMUM), v, true);
+    MIDIOUT.sendNoteOn(t, v, MIDI_CHANNEL_BITBOX_OUT);  // also send trigger for the envelopes
+  } else if (p == MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
+    //Serial.printf(">> got BASS trigger!\r\n");
+    //if (bass.is_note_held()) // todo: make this so that can still play bass when no DAW present...
+    bass_note_on_and_next();
+    //else
+    //  Serial.println("No note held? is_note_held is false");
+  } else {
+    Serial.printf("WARNING: fire_trigger not doing anything with pitch %i\r\n", p);
+  }
+  if (midiecho_enabled)
+    if (internal) echo_fire_trigger(p - MUSO_NOTE_MINIMUM, v);
 }
 
 void douse_trigger(byte t, byte v = 0, bool internal = false) {
-    byte p = MUSO_NOTE_MINIMUM + t;
-    if (
-      p>=MUSO_NOTE_MINIMUM && 
-      p<MUSO_NOTE_MAXIMUM) {
-        trigger_status[p - MUSO_NOTE_MINIMUM] = TRIGGER_IS_OFF;
-        MIDIOUT.sendNoteOff(p, v, MUSO_GATE_CHANNEL);   // hardcoded channel 16 for midimuso
-    } else if (
-      p>=MUSO_NOTE_MAXIMUM && 
-      p<MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
-        update_envelope (p - (MUSO_NOTE_MAXIMUM), 0, false);
-    } else if (p == MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
-      bass_note_off();
-    } else {
-      Serial.printf("WARNING: douse_trigger not doing anything with pitch %i\r\n", p);
-    }
-    if (midiecho_enabled)
-      if (internal) echo_douse_trigger(p-MUSO_NOTE_MINIMUM, v);
+  byte p = MUSO_NOTE_MINIMUM + t;
+  if (
+    p >= MUSO_NOTE_MINIMUM &&
+    p < MUSO_NOTE_MAXIMUM) {
+    trigger_status[p - MUSO_NOTE_MINIMUM] = TRIGGER_IS_OFF;
+    MIDIOUT.sendNoteOff(p, v, MUSO_GATE_CHANNEL);   // hardcoded channel 16 for midimuso
+    MIDIOUT.sendNoteOff(t, v, MIDI_CHANNEL_BITBOX_OUT);
+  } else if (
+    p >= MUSO_NOTE_MAXIMUM &&
+    p < MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
+    update_envelope (p - (MUSO_NOTE_MAXIMUM), 0, false);
+    MIDIOUT.sendNoteOff(t, v, MIDI_CHANNEL_BITBOX_OUT);
+  } else if (p == MUSO_NOTE_MAXIMUM + NUM_ENVELOPES) {
+    bass_note_off();
+  } else {
+    Serial.printf("WARNING: douse_trigger not doing anything with pitch %i\r\n", p);
+  }
+  if (midiecho_enabled)
+    if (internal) echo_douse_trigger(p - MUSO_NOTE_MINIMUM, v);
 }
 
 void douse_all_triggers(bool internal = false) {
@@ -62,9 +68,14 @@ void douse_all_triggers(bool internal = false) {
 
 void midi_send_envelope_level(byte envelope, byte level) {
   //Serial.printf("Envelope[%i] in stage %i: sending lvl %i to midi_cc %i!\r\n", envelope, envelopes[envelope].stage, level, envelopes[envelope].midi_cc);
-  if (envelope==ENV_RIDE_CYMBAL) {  // hack to use the pitch bend output as an envelope, since my 'cc 74' output seems to have stopped working - could use this to add an extra envelope or LFO etc
-    MIDIOUT.sendPitchBend((level<<7) + MIDI_PITCHBEND_MIN, MUSO_CV_CHANNEL);
+  //if (envelope == ENV_RIDE_CYMBAL) { // hack to use the pitch bend output as an envelope, since my 'cc 74' output seems to have stopped working - could use this to add an extra envelope or LFO etc
+#ifdef MUSO_USE_PITCH_FOR
+  if (envelopes[envelope].midi_cc == MUSO_USE_PITCH_FOR) {
+    MIDIOUT.sendPitchBend((level << 7) + MIDI_PITCHBEND_MIN, MUSO_CV_CHANNEL);
   } else {
+#else
+  {
+#endif
     MIDIOUT.sendControlChange(envelopes[envelope].midi_cc, level, MUSO_CV_CHANNEL); // send message to midimuso
   }
 }
@@ -73,7 +84,7 @@ void midi_bass_send_note_on(int pitch, int velocity, int channel) {
   MIDIOUT.sendNoteOn(pitch, velocity, channel);
   if (midiecho_enabled)
     MIDIIN.sendNoteOn(pitch, velocity, MIDI_CHANNEL_BASS_OUT);  // echo back to host
-  //Serial.printf("midi_bass_send_note_on(%i, %i, %i)\n", pitch, velocity, MIDI_CHANNEL_BASS_OUT);  
+  //Serial.printf("midi_bass_send_note_on(%i, %i, %i)\n", pitch, velocity, MIDI_CHANNEL_BASS_OUT);
   // todo: move echo back to host stuff into MidiEcho
 }
 
@@ -85,14 +96,15 @@ void midi_bass_send_note_off(int pitch, int velocity, int channel) {
 }
 
 void midi_kill_notes() {
-  MIDIOUT.sendControlChange(123,0,MUSO_GATE_CHANNEL); // todo -- check what this is actually doing/meant to do?!
-  MIDIOUT.sendControlChange(123,0,MIDI_CHANNEL_BASS_OUT); // todo -- check what this is actually doing/meant to do?!
-  MIDIIN.sendControlChange(123,0,MIDI_CHANNEL_BASS_OUT); // todo -- check what this is actually doing/meant to do?!
-  MIDIIN.sendControlChange(123,0,GM_CHANNEL_DRUMS);
+  MIDIOUT.sendControlChange (123, 0, MUSO_GATE_CHANNEL);   // 123 = kill all notes
+  MIDIOUT.sendControlChange (123, 0, MIDI_CHANNEL_BASS_OUT); // 123 = kill all notes
+  MIDIOUT.sendControlChange (123, 0, MIDI_CHANNEL_BITBOX_OUT); // 123 = kill all notes
+  MIDIIN.sendControlChange  (123, 0, MIDI_CHANNEL_BASS_OUT); // 123 = kill all notes - for midiecho to host
+  MIDIIN.sendControlChange  (123, 0, GM_CHANNEL_DRUMS);     // 123 = kill all notes - for midiecho to host
 }
 
 void kill_notes() {
-  // forget which triggers are active 
+  // forget which triggers are active
   douse_all_triggers(true);
   //activeNotes = 0;
 
@@ -101,7 +113,7 @@ void kill_notes() {
 
 void midi_send_clock(unsigned long received_ticks) {
   static unsigned long last_clock_ticked;
-  if (received_ticks!=last_clock_ticked)
+  if (received_ticks != last_clock_ticked)
     MIDIOUT.sendClock();
   last_clock_ticked = received_ticks;
 }
